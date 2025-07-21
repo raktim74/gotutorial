cb54407a8ff9258afcaa1c791dd765e135f3f29b
Raktim Nath <raktim.nath@carvewing.com>
2025-07-21 22:29:47 +0530

    get method check added

--- chapter4/main.go
+++ chapter4/main.go
@@ -63,6 +63,11 @@ func main() {
 }
 
 func getUsers(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
 	rows, err := db.Query("select * from tgt.newtable")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
